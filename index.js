fabdc28065c401f4e6efc0072839c77cb4a178db
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-08-29 22:34:06 +0530

    click me, increase the size of para and navbar.

---
+++ index.js
@@ -0,0 +1,34 @@
+// class Animal {
+//     static belly = [];
+//     eat() {
+//       Animal.belly.push('food');
+//     }
+//   }
+//   let a = new Animal();
+//   a.eat();
+//   console.log(a.belly[0],"++++++++++++++++++++++++++++++++++++++++++++");
+
+
+
+// var Storm = function () {};
+// Storm.prototype.precip = 'rain';
+// var WinterStorm = function () {};
+// WinterStorm.prototype = new Storm();
+// WinterStorm.prototype.precip = 'snow';
+// var bob = new WinterStorm();
+// console.log(bob.precip,"+++++++++++++++++++++++++++++++++++++++++++++++");
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(e) {
+    alert("Button clicked!")
+});
+
+let btnInc = document.getElementById("btn-Inc");
+let para = document.getElementById("para")
+let fontSize = 16
+btnInc.addEventListener("click", function(e) {
+
+para.style.fontSize = `${fontSize}px`;
+fontSize = fontSize + 2;
+})
